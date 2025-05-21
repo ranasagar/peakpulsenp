7de73ac9839b2e08e63556dfd0cccf81b00856bc
Personally Personal <sagarrana@gmail.com>
2025-05-21 18:25:07 +0000

    Fix: Remove obsolete (main)/layout.tsx to resolve build error

--- src/app/(main)/layout.tsx
+++
@@ -1,4 +0,0 @@
-// This file has been removed.
-// The main layout functionality was moved to /src/components/layout/main-layout.tsx
-// and is used by /src/app/page.tsx and other relevant pages directly.
-// If you see this file, please ensure it is deleted from your project and Git repository.
