9f35949d4a32cc24a4fb5da53d36c4c62a336190
Personally Personal <sagarrana@gmail.com>
2025-05-22 14:24:29 +0000

    Intermediate: Prepare (main) directory for deletion

--- src/app/(main)/accessibility/page.tsx
+++
@@ -1,3 +0,0 @@
-// This file is obsolete and its content has been moved to /src/app/accessibility/page.tsx
-// Please delete the entire /src/app/(main)/ directory from your project and Git repository.
-export {};
--- src/app/(main)/careers/page.tsx
+++
@@ -1,5 +0,0 @@
-// This file is obsolete. The content for the /careers page
-// is now served by /src/app/careers/page.tsx.
-// This old file (and the entire /src/app/(main)/ directory if all pages have been moved)
-// should be deleted from your Git repository.
-export {};
